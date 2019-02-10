f578ea78cc3f08af81f5075cca30ac0746d61a17
Kathryn Hampton <khampton119@gmail.com>
2019-02-10 08:56:13 -0500

    restarting project

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import Dogs from './Components/dogs';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button 
+          onPress={() => {console.log("pressed button")}}
+          title="Press me!"
+        />
+        <View>
+          <Dogs />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paragraph: {
+    margin: 20,
+    fontSize: 18,
+    textAlign: 'center'
+  },
+});
+
+const { container, paragraph } = styles;
---
+++ Components/dogs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default Dogs = () => {
+  return (
+    <View>
+      <Text>Hello! This will return super cute dogs!</Text>
+    </View>
+  )
+}
